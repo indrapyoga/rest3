8f316f9425a79f8b0f2d297b9013b7898487f4b3
DESKTOP-37Q3L3S\Indra Prayoga S <indraprayogas21@gmail.com>
2022-02-17 23:11:37 +0700

    tambahan sqlite

--- rest3.py
+++ rest3.py
@@ -1,24 +1,66 @@
 from flask import Flask,request,jsonify
 from flask_restful import Resource, Api
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 
+#Inisialisasi library
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
+#konfigurasi database
+db = SQLAlchemy(app)
+basedir = os.path.dirname(os.path.abspath(__file__))
+database = "sqlite:///" + os.path.join(basedir, "db.sqlite")
+app.config["SQLAlchemy_DATABASE_URI"] = database
+
+db.create_all()
+
+#database model
+class ModelDatabase(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    nama = db.Column(db.String(100))
+    nim = db.Column(db.Integer)
+    #method menyimpan
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return True
+        except:
+            return False
+
+db.create_all()
+#variabel global
 identitas = {}
 
 class KResource(Resource):
     def get(self):
-        return identitas
+        query = ModelDatabase.query.all()
+        
+        output = [{
+            "nama":data.nama,
+            "nim":data.nim  
+        } for data in query
+        ]
+        
+        response = {
+            "data":output 
+        }
+        return response
+        
     
     def post(self):
         dataNama = request.form["nama"]
         dataNim = request.form["nim"]
-        identitas["nama"] = dataNama
-        identitas["nim"] = dataNim
-        response = {"msg":"berhasil"}
+        
+        #masukkan data ke model
+        model = ModelDatabase(nama=dataNama,nim=dataNim)
+        model.save()
+        
+        response = {"msg":"berhasil ditambah"}
         return response, 200
 
 api.add_resource(KResource,"/api",methods=["GET","POST"])
